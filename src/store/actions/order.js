c505b0523fb78a52c269a52b0942331c5d4e0f62
Jeff Wagner <jeff@moonrise.works>
2021-01-01 13:13:54 -0600

    use redux for Orders management

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -21,6 +21,26 @@ export const purchaseBurgerStart = () => {
 	}
 };
 
+export const fetchOrdersSuccess = (orders) => {
+	return {
+		type: actionTypes.FETCH_ORDERS_SUCCESS,
+		orders,
+	}
+};
+
+export const fetchOrdersFail = (error) => {
+	return {
+		type: actionTypes.FETCH_ORDERS_FAIL,
+		error,
+	}
+};
+
+export const fetchOrdersStart = () => {
+	return {
+		type: actionTypes.FETCH_ORDERS_START,
+	}
+};
+
 export const purchaseInit = () => {
 	return {
 		type: actionTypes.PURCHASE_INIT,
@@ -39,3 +59,23 @@ export const purchaseBurger = (orderData) => {
 		});
 	}
 };
+
+export const fetchOrders = () => {
+	return dispatch => {
+		dispatch(fetchOrdersStart());
+		axios.get('/orders.json')
+		.then((res) => {
+			const fetchedOrders = [];
+			for (const key in res.data) {
+				fetchedOrders.push({
+					...res.data[key], 
+					id: key,
+				});
+			}
+			dispatch(fetchOrdersSuccess(fetchedOrders));
+		})
+		.catch((err) => {
+			dispatch(fetchOrdersFail(err));
+		});
+	};
+}
